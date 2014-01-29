d892d7a3aeaf39fa4e3b4a0c7034f241b8c57453
Stephen Wright <stephen.charles.wright@gmail.com>
2014-01-29 22:18:47 +0000

    continued working on home page

--- public/js/controllers/application_controller.js
+++ public/js/controllers/application_controller.js
@@ -2,7 +2,11 @@ var ApplicationController = Ember.ObjectController.extend({
 	needs: 'sidebar.user',
 	isAuthenticated: function() {
 		return App.Session.isAuthenticated()
-	}.property('App.Session.token')
+	}.property('App.Session.token'),
+	profile: function() {
+		var user = this.store.find('profile', App.Session.get('uid'));
+		return user;
+	}.property()
 });
 
 module.exports = ApplicationController;
