ebb2b2a86f49751906cca771d7e600fc4bd257ef
Stephen Wright <ste.c.wr@gmail.com>
2014-01-29 16:59:38 +0000

    changes

--- public/js/application.js
+++ public/js/application.js
@@ -988,7 +988,7 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   
-  data.buffer.push("Brand");
+  data.buffer.push("Intripd");
   }
 
 function program3(depth0,data) {
