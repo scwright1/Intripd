e5965310ce0e255eccea53f4c6234ffb55855835
Stephen Wright <stephen.charles.wright@gmail.com>
2014-02-01 17:12:31 +0000

    completed refactoring on routes.js

--- public/js/application.js
+++ public/js/application.js
@@ -29,18 +29,9 @@ App.Router.map(function() {
 		this.route('login');
 		this.route('register');
 	}); 
-
 	this.route('map');
-
 	this.route('tos');
 	this.route('privacy');
-
-	//this.resource('marker');
-
-	this.resource('sidebar', function() {
-		this.route('user');
-		this.route('trips');
-	});
 });
 
 
