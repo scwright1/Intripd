5c6c4bedcb583758fb625c9654cf3b1d1f995d77
Stephen Wright <stephen.charles.wright@gmail.com>
2014-03-05 21:37:18 +0000

    refactored server code more.  Now should have appropriate try/catch methods

--- server.js
+++ server.js
@@ -1,66 +1,69 @@
-/* Node.js entry */
+/************************************************************/
+/*															*/
+/*	Intripd.com												*/
+/*															*/
+/*	(c) 2014 Stephen Wright <ste.c.wr@gmail.com				*/
+/*	All rights reserved.									*/
+/*															*/
+/*	filename: server.js										*/
+/*	version: 2.0.1											*/
+/*															*/
+/************************************************************/
 
-var express    = require('express'),
-    mongoose   = require('mongoose'),
-    mongodb    = require('mongodb'),
-    passport   = require('passport'),
-    config     = require('./config')(),
-    User       = require('./app/models/usermodel'),
-    flox       = require('flox-node')({}),
-    server     = express();
+var express		= require('express'),
+	mongodb		= require('mongodb'),
+	mongoose	= require('mongoose'),
+	passport	= require('passport'),
+	config		= require('./config')(),
+	User		= require('./app/models/usermodel');
 
-//start the server
-server.listen(config.port, function() {
-  if(config.mode === "development") {
-    console.log('Intripd starting on port ' + config.port);
-  }
+//setup debugging
+require('flox-node')({});
+
+//link express to the server
+var server = express();
+
+//unhandledException catch for methods that fall through try/catch
+process.on('unhandledException', function(err) {
+	console.error('Caught unhandled Exception ' + err);
 });
 
-//export token_KEY='<[63Y4!29R8NZ<Q36@iJX3)QrSPr11'
+//start the server
+try {
+	server.listen(config.port, function(){console.log('Intripd starting on port ' + config.port);});
+} catch(e) {
+	console.error('Failed to start Server on port ' + config.port + ' due to ' + e);
+}
+
 //connect to Mongo Database and check that we've connected OK.
 mongoose.connect('mongodb://' + config.mongo.host + '/' + config.mongo.db);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
-if(config.mode === "development") {
-  db.once('open', function callback() {
-    console.log('Mongoose connection open at ' + db.host + ':' + db.port);
-    console.log('Using database ' + db.name);
-  });
-}
+db.once('open', function callback() {
+	console.log('Mongoose connection open at ' + db.host + ':' + db.port);
+	console.log('Using database ' + db.name);
+});
 
 //set up passport configuration prior to initialize
 require('./app/controllers/passport')(passport);
 
 server.use(express.static(__dirname + '/public'));
-//DEPRECATED - functionality removed from express 4.0.0
-//server.use(express.logger('dev'));
-//server.use(express.json());
-//server.use(express.urlencoded());
-//server.use(express.cookieParser('qL17C8iQnxPuDg50mYFDk56sdR0KuUm3'));
 server.use(require('morgan')('dev'));
 server.use(require('body-parser')());
 server.use(require('method-override')());
-//use cookieparser for session storage
-server.use(require('cookie-parser')('qL17C8iQnxPuDg50mYFDk56sdR0KuUm3'));
+
 //initialize passport with config options defined above
 server.use(passport.initialize());
 server.use(passport.session());
 
 //load the routers
-require('./app/router/base.js')(server);
-require('./app/router/auth.js')(server, passport);
-require('./app/router/map-root.js')(server);
-require('./app/router/sessions.js')(server);
-require('./app/router/user.js')(server);
-require('./app/router/trip.js')(server);
-require('./app/router/waypoint.js')(server);
-
-//flowerbox
-console.log('==============================');
-console.log('=                            =');
-console.log('= App Started Successfully!  =');
-console.log('= Intripd "geneva" release.  =');
-console.log('= (c) 2014 Intripd.          =');
-console.log('= All Rights Reserved.       =');
-console.log('=                            =');
-console.log('==============================');
+setTimeout(function() {
+	require('./app/router/base.js')(server);
+	require('./app/router/auth.js')(server, passport);
+	require('./app/router/map-root.js')(server);
+	require('./app/router/sessions.js')(server);
+	require('./app/router/user.js')(server);
+	require('./app/router/trip.js')(server);
+	require('./app/router/waypoint.js')(server);
+	console.log('Routers loaded');
+}, 50);
