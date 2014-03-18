9ba5a827566296d652d13fb44669d97eb4bb6476
Stephen Wright <ste.c.wr@gmail.com>
2014-03-18 17:05:52 +0000

    started working on rewrite for uncaught exceptions

--- core/server/index.js
+++ core/server/index.js
@@ -5,8 +5,18 @@
 var express			= require('express'),
 	config			= require('./config');
 
+process.on('uncaughtException', function(err, req, res, next) {
+	console.error('UNHANDLED EXCEPTION: ' + err.message);
+	console.error('========== START STACK =========');
+	console.error(err.stack);
+	console.error('==========  END STACK  =========');
+	process.exit(1);
+});
+
 function boot(server) {
-	console.log('Hi');
+	setTimeout(function () {
+      throw new Error('User generated fault.');
+   },5000);
 }
 
 function init() {
