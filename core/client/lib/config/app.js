130b78097283c1bf8ae3fb6339860020b44e4a8c
Stephen Wright <ste.c.wr@gmail.com>
2014-03-31 19:41:39 +0100

    removed javascript vendor libraries in favour of CDNs

--- core/client/lib/config/app.js
+++ core/client/lib/config/app.js
@@ -1,9 +1,9 @@
-require('../vendor/jquery.min');
-require('../vendor/jquery.cookie');
-require('../vendor/bootstrap-datepicker');
-require('../vendor/handlebars');
-require('../vendor/ember');
-require('../vendor/ember-data');
+//require('../vendor/jquery.min');
+//require('../vendor/jquery.cookie');
+//require('../vendor/bootstrap-datepicker');
+//require('../vendor/handlebars');
+//require('../vendor/ember');
+//require('../vendor/ember-data');
 
 window.App = Ember.Application.create();
 
@@ -18,3 +18,47 @@ App.Store = DS.Store.extend({
 });
 
 module.exports = App;
+
+
+/*
+registerImplementation of hashbang url
+ */
+
+(function() {
+
+var get = Ember.get, set = Ember.set;
+
+Ember.Location.registerImplementation('hashbang', Ember.HashLocation.extend({ 
+
+    getURL: function() {
+        return get(this, 'location').hash.substr(2);
+    },
+
+    setURL: function(path) {
+        get(this, 'location').hash = "!"+path;
+        set(this, 'lastSetURL', "!"+path);
+    },
+
+    onUpdateURL: function(callback) {
+        var self = this;
+        var guid = Ember.guidFor(this);
+
+        Ember.$(window).bind('hashchange.ember-location-'+guid, function() {
+                Ember.run(function() {
+                    var path = location.hash.substr(2);
+                    if (get(self, 'lastSetURL') === path) { return; }
+
+                    set(self, 'lastSetURL', null);
+
+                    callback(location.hash.substr(2));
+                });
+        });
+    },
+
+    formatURL: function(url) {
+        return '#!'+url;
+    }
+
+}));
+
+})();
