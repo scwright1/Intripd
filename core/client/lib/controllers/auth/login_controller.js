5c922919646489d5c37045c67b0fbef53817c732
Stephen Wright <ste.c.wr@gmail.com>
2014-04-10 16:26:07 +0100

    finished login layout.  Need logic

---
+++ core/client/lib/controllers/auth/login_controller.js
@@ -0,0 +1,10 @@
+var AuthLoginController = Ember.ObjectController.extend({
+	remember: false,
+	actions: {
+		login: function() {
+			alert('no login function here yet');
+		}
+	}
+});
+
+module.exports = AuthLoginController;
