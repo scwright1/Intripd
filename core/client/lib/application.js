ab04a0f329e2786ca52b3e094dad8f7af8b9bad5
Stephen Wright <ste.c.wr@gmail.com>
2014-06-20 13:57:36 +0100

    #11 - sidebars working fully.  Resizing with window and changing scale correctly

--- core/client/lib/application.js
+++ core/client/lib/application.js
@@ -1604,60 +1604,91 @@ var MapView = Em.View.extend({
 
 module.exports = MapView;
 },{}],32:[function(require,module,exports){
+/**
+ * Sidebar/trigger_view.js
+ * View logic for the sidebar menu items to allow for insertion of the primary menus
+ * Created: 19/Jun/2014
+ */
+
 var TriggerView = Em.View.extend({
-	active: false,
 	classNames: ['menu-item'],
 	classNameBindings: ['active'],
 	template: Ember.Handlebars.compile("<div {{bind-attr class='view.icon'}}></div>"),
 	click: function() {
-		var $menu = $('#sidebar-menu'),
-			_this = this;
-		if($menu.hasClass('active')) {
-			if(_this.get('active')) {
+
+		var $m = $('#sidebar-menu'),
+			$c = $('#map-canvas'),
+			$t = $('#topbar-menu'),
+			_t = this;
+
+		if($m.hasClass('active')) {
+			if(this.get('active')) {
 				//this is the current active menu, so close
 				this.set('active', false);
-				$menu.data('fill', false);
-				$menu.removeClass('active');
-				$menu.animate({'left': (80 - $menu.width())+'px'}, {duration: 400, queue: false});
-				$('#map-canvas').animate({'left': '80px'}, {duration: 400, queue: false, step: function() {
-					google.maps.event.trigger(_this.get('controller.controllers.map').get('map'), 'resize');
+				$m.data('fill', false);
+				$m.removeClass('active');
+				$m.animate({'left': (80 - $m.width())+'px'}, {duration: 400, queue: false});
+				$c.animate({'left': '80px'}, {duration: 400, queue: false, step: function() {
+					google.maps.event.trigger(_t.get('controller.controllers.map').get('map'), 'resize');
 				}});
 			} else {
-				this.set('active', false);
-				//new menu, change the content
-				for(var i = 0; i < _this.get('parentView').get('childViews').length; i++) {
-					_this.get('parentView').get('childViews').objectAt(i).set('active', false);
+				//change the currently active menu
+				for(var i = 0; i < _t.get('parentView').get('childViews').length; i++) {
+					_t.get('parentView').get('childViews').objectAt(i).set('active', false);
 				}
-				$menu.animate({'width': _this.get('width')+'px'}, {duration: 400, queue: false});
-				$('#map-canvas').animate({'left': _this.get('width')+'px'}, {duration: 400, queue: false, step: function() {
-					google.maps.event.trigger(_this.get('controller.controllers.map').get('map'), 'resize');
-				}});
-				if(this.get('menu_context') !== '') {
-					this.get('controller').send('renderMenuElement', this.get('menu_context'), 'sidebar-menu');
+				this.set('active', true);
+
+				if(this.get('width') === 'full') {
+					$m.data('fill', true);
+					//transition to full screen width
+					if($t.hasClass('active')) {
+						//fill up to social sidebar
+						var w = ($(document).width() - ($t.width() + 81));
+						var l = ($(document).width() - $t.width());
+						$m.animate({'width': w+'px'}, {duration: 400, queue: false});
+						$c.animate({'left': l+'px'}, {duration: 400, queue: false, step: function() {
+							google.maps.event.trigger(_t.get('controller.controllers.map').get('map'), 'resize');
+						}});
+					} else {
+						//social sidebar not open, so fill width
+						var w = ($(document).width() - 80);
+						$m.animate({'width': w+'px'}, {duration: 400, queue: false});
+						$c.animate({'left': $(document).width()+'px'}, {duration: 400, queue: false, step: function() {
+							google.maps.event.trigger(_t.get('controller.controllers.map').get('map'), 'resize');
+						}});
+					}
+				} else {
+					$m.data('fill', false);
+					var l = parseInt(this.get('width')) + 80;
+					$m.animate({'width': _t.get('width')+'px'}, {duration: 400, queue: false});
+					$c.animate({'left': l+'px'}, {duration: 400, queue: false, step: function() {
+						google.maps.event.trigger(_t.get('controller.controllers.map').get('map'), 'resize');
+					}});
 				}
+
 			}
 		} else {
 			var w;
 			this.set('active', true);
 			//open the menu
 			this.get('controller').send('renderMenuElement', this.get('menu_context'), 'sidebar-menu');
-			$menu.addClass('active');
+			$m.addClass('active');
 			if(this.get('width') === 'full') {
-				if($('#topbar-menu').hasClass('active')) {
-					w = $(document).width() - ($('#topbar-menu').width() + 81);
+				$m.data('fill', true);
+				if($t.hasClass('active')) {
+					w = $(document).width() - ($t.width() + 81);
 				} else {
 					w = $(document).width() - 80;
 				}
 			} else {
 				w = this.get('width');
 			}
-			$menu.css('left', (80-w)+'px');
-			$menu.css('width', w+'px');
-			$menu.data('fill', true);
-			$menu.animate({'left': '80px'}, {duration: 400, queue: false});
+			$m.css('left', (80-w)+'px');
+			$m.css('width', w+'px');
+			$m.animate({'left': '80px'}, {duration: 400, queue: false});
 			var ml = parseInt(w) + 80;
-			$('#map-canvas').animate({'left': ml+'px'}, {duration: 400, queue: false, step: function() {
-				google.maps.event.trigger(_this.get('controller.controllers.map').get('map'), 'resize');
+			$c.animate({'left': ml+'px'}, {duration: 400, queue: false, step: function() {
+				google.maps.event.trigger(_t.get('controller.controllers.map').get('map'), 'resize');
 			}});
 		}
 	}
